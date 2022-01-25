4ed7e546f1be318283b3d9ebcf3f7aa957e4ea94
Wastelander22 <afejes22@gmail.com>
2022-01-25 23:02:18 +0100

    Initial commit

---
+++ src/Palindrome.java
@@ -0,0 +1,37 @@
+public class Palindrome {
+
+    public static void main(String[] args) {
+        // Develop a program that checks if a string entered by a user is palindrome.
+        //(A palindrome is a word that reads the same forwards as backwards. An example is “racecar” or “madam”.)
+
+        isPalindrom("racecar");
+        isPalindrom("programming");
+
+    }
+    public static boolean isPalindrom(String str) {
+
+        System.out.println("the word: " + str);
+
+        StringBuffer sb = new StringBuffer();
+
+        sb.append(str);
+
+        //System.out.println("sb.append(str): "+sb.append(str));
+
+        StringBuffer str2 = sb.reverse();
+
+        //System.out.println("str: "+str2);
+
+        String str3 = str2.toString();//ha ez a lépés nincs akkor lesz egy:
+        //:"Unlikely argument type for equals(): StringBuffer seems to be unrelated to String" warning az if statementben
+
+        if(str.equals(str3)) {
+            System.out.println("This is palindrom");
+            return true;
+        }else {
+            System.out.println("This is not a palindrom, because the reverse is: " + str2);
+
+            return false;
+        }
+    }
+}
